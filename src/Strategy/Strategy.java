bd3610aced8d61abe0cf55094f4f80bb06f51518
fondawu <34894570@qq.com>
2019-03-20 15:40:05 +0800

    Initial commit

---
+++ src/Strategy/Strategy.java
@@ -0,0 +1,11 @@
+package Strategy;
+
+/**
+ * Created by fondawu on 2019/3/20.
+ */
+public class Strategy {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
